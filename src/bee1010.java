efbf3add611f7ae1734cd7cbffbdbd0ea96294f9
fcdias0812 <fcdias.main@gmail.com>
2026-03-03 20:55:59 -0300

    feat: resolve exercicio 1010

---
+++ src/bee1010.java
@@ -0,0 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class bee1010 {
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int codeProduct1 = sc.nextInt();
+        int unitsProduct1 = sc.nextInt();
+        double priceProduct1 = sc.nextDouble();
+
+        int codeProduct2 = sc.nextInt();
+        int unitsProduct2 = sc.nextInt();
+        double priceProduct2 = sc.nextDouble();
+
+        double total = unitsProduct1 * priceProduct1 + unitsProduct2 * priceProduct2;
+
+        System.out.printf("VALOR A PAGAR: R$ %.2f", total);
+        System.out.println();
+
+        sc.close();
+    }
+}
